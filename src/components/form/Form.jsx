95965c9821fa354689d9d76aaf5249775cc61b16
Nikolai-desig <rinzleramad79@gmail.com>
2023-06-19 18:37:52 +0300

    патерн

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -61,7 +61,6 @@ function Phonebook() {
           onChange={handleChange}
           placeholder="Enter Name"
           name="name"
-          pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
           required
         />
       </label>
@@ -75,7 +74,7 @@ function Phonebook() {
           onChange={handleChange}
           placeholder="Enter Number"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
